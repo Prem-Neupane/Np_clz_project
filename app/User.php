29537bee23906f297aff4ccd30a69007cfcd273c
San Neupane <123here.comes.san@gmail.com>
2018-09-30 22:11:08 +0545

    added gender

--- app/User.php
+++ app/User.php
@@ -11,7 +11,7 @@ class User extends Authenticatable
     use Notifiable;
   
     protected $fillable = [
-        'first_name','last_name','username','email', 'password','identity'
+        'first_name','last_name','username','email', 'password','identity','gender'
     ];
 
     protected $hidden = [
