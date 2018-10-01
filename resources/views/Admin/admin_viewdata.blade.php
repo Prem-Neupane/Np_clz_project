f1a53d4926a9a16c8653a82d29676cf0e944e8fd
San Neupane <123here.comes.san@gmail.com>
2018-10-01 23:41:25 +0545

    boostrap table: align center added

--- resources/views/Admin/admin_viewdata.blade.php
+++ resources/views/Admin/admin_viewdata.blade.php
@@ -22,36 +22,36 @@
 						<table data-toggle="table">
 						    <thead>
 						    <tr>
-						        <th data-field="id" style="text-align: center">ID</th>
-						        <th data-field="name" style="text-align: center">Name</th>
-						        <th data-field="username" style="text-align: center">Username</th>
-						        <th data-field="email" style="text-align: center">Email-id</th>
-						        <th data-field="role" style="text-align: center">Role</th>
-						        <th data-field="gender" style="text-align: center">Gender</th>
-						        <th data-field="status" style="text-align: center">Status</th>
-						        <th data-field="action" style="text-align: center" colspan="2">Actions</th>
+						        <th data-field="id" >ID</th>
+						        <th data-field="name" >Name</th>
+						        <th data-field="username" >Username</th>
+						        <th data-field="email" >Email-id</th>
+						        <th data-field="role" >Role</th>
+						        <th data-field="gender" >Gender</th>
+						        <th data-field="status" >Status</th>
+						        <th data-field="action"  colspan="2">Actions</th>
 						    </tr>
 						    </thead>
 						    @foreach($users as $user)
 							    <tr>
-							    	<td style="text-align: center">{{ $user->id }}</td>
-							    	<td style="text-align: center">{{ $user->first_name }}</td>
-					    			<td style="text-align: center">{{ $user->username }}</td>
-							    	<td style="text-align: center">{{ $user->email }}</td>
-							    	<td style="text-align: center">{{ $user->identity }}</td>
+							    	<td >{{ $user->id }}</td>
+							    	<td >{{ $user->first_name }}</td>
+					    			<td >{{ $user->username }}</td>
+							    	<td >{{ $user->email }}</td>
+							    	<td >{{ $user->identity }}</td>
 							    	@if($user->gender == 0)
-							    		<td style="text-align: center">Female</td>
-							    	@else <td style="text-align: center"> Male </td>
+							    		<td >Female</td>
+							    	@else <td > Male </td>
 							    	@endif
 
 							    	@if($user->active == 0)
-							    		<td style="text-align: center"><button type="button" class = 'btn btn-danger'>inactive</button></td>
-							    	@else <td style="text-align: center"> <button class = 'btn btn-success'>active</button> </td>
+							    		<td ><button type="button" class = 'btn btn-danger'>inactive</button></td>
+							    	@else <td > <button class = 'btn btn-success'>active</button> </td>
 							    	@endif
 
 
-							    	<td style="text-align: center"><button type="button" class = 'btn btn-primary'>Update</button></td>
-							    	<td style="text-align: center"><button type="button" class = 'btn btn-danger'>Delete</button></td>
+							    	<td ><button type="button" class = 'btn btn-primary'>Update</button></td>
+							    	<td ><button type="button" class = 'btn btn-danger'>Delete</button></td>
 							    		
 							    </tr>
 						    @endforeach
