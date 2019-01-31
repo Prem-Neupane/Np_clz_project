39a654a6cbb17f814f5456d2d4cf778d1f9fde0b
Bishwash <bishwash.pa@gmail.com>
2019-01-31 13:20:47 +0545

    composer is updated

--- vendor/laravel/framework/src/Illuminate/Auth/SessionGuard.php
+++ vendor/laravel/framework/src/Illuminate/Auth/SessionGuard.php
@@ -135,9 +135,7 @@ class SessionGuard implements StatefulGuard, SupportsBasicAuth
         // If the user is null, but we decrypt a "recaller" cookie we can attempt to
         // pull the user data on that cookie which serves as a remember cookie on
         // the application. Once we have a user we can return it to the caller.
-        $recaller = $this->recaller();
-
-        if (is_null($this->user) && ! is_null($recaller)) {
+        if (is_null($this->user) && ! is_null($recaller = $this->recaller())) {
             $this->user = $this->userFromRecaller($recaller);
 
             if ($this->user) {
@@ -329,6 +327,7 @@ class SessionGuard implements StatefulGuard, SupportsBasicAuth
      * Get the response for basic authentication.
      *
      * @return void
+     *
      * @throws \Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException
      */
     protected function failedBasicResponse()
@@ -493,7 +492,7 @@ class SessionGuard implements StatefulGuard, SupportsBasicAuth
         }
 
         if (isset($this->events)) {
-            $this->events->dispatch(new Events\Logout($this, $user));
+            $this->events->dispatch(new Events\Logout($this->name, $user));
         }
 
         // Once we have fired the logout event we will clear the users out of memory
@@ -580,7 +579,7 @@ class SessionGuard implements StatefulGuard, SupportsBasicAuth
     {
         if (isset($this->events)) {
             $this->events->dispatch(new Events\Attempting(
-                $this, $credentials, $remember
+                $this->name, $credentials, $remember
             ));
         }
     }
@@ -596,7 +595,7 @@ class SessionGuard implements StatefulGuard, SupportsBasicAuth
     {
         if (isset($this->events)) {
             $this->events->dispatch(new Events\Login(
-                $this, $user, $remember
+                $this->name, $user, $remember
             ));
         }
     }
@@ -611,7 +610,7 @@ class SessionGuard implements StatefulGuard, SupportsBasicAuth
     {
         if (isset($this->events)) {
             $this->events->dispatch(new Events\Authenticated(
-                $this, $user
+                $this->name, $user
             ));
         }
     }
@@ -627,7 +626,7 @@ class SessionGuard implements StatefulGuard, SupportsBasicAuth
     {
         if (isset($this->events)) {
             $this->events->dispatch(new Events\Failed(
-                $this, $user, $credentials
+                $this->name, $user, $credentials
             ));
         }
     }
